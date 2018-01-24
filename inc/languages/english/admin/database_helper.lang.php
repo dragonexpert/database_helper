8acd64aa8afcb7e54bfcd7aadd39faa2d8435996
dragonexpert <arceus@teamdimensional.net>
2018-01-24 02:32:31 -0600

    Create database_helper.lang.php

---
+++ inc/languages/english/admin/database_helper.lang.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Latios
+ * Date: 1/23/2018
+ * Time: 10:31 PM
+ */
+$l['database_helper'] = "Database Helper";
+$l['database_helper_description'] = "Adds some useful features to the database class.";
+$l['database_helper_delete'] = "You must explicitly call the delete_query method.";
+$l['database_helper_drop'] = "You must explicitly call the drop_table method.";
+$l['database_helper_truncate'] = "You must explicitly call the truncate_table method.";
